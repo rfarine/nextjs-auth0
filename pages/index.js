891daa66ccf6ffd887f576bd7ac94addff3e8f17
Rae Farine <rfarine1027@gmail.com>
2018-07-23 23:39:13 -0400

    Add NextJS

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const Index = () => (
+  <div>
+    <Head>
+      <title>NextJS + Auth0</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <p>Hello world!</p>
+  </div>
+)
+
+export default Index
